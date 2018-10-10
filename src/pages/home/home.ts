cbfe211c1ed8ccdf85700247b08234adec0868fb
Student <student@iMac-B202-04.local>
2018-10-10 12:45:47 +0200

    Translation provider added Translation added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { TranslationProvider } from '../../providers/translation/translation';
 
 @Component({
   selector: 'page-home',
@@ -8,12 +9,17 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   private userInput:string;
-
-  constructor(public navCtrl: NavController) {
+  private result:string;
+  constructor(public navCtrl: NavController, private translationProvider:TranslationProvider) {
    
   }
   public btnTranslateClicked(userInput:string):void{
     console.log(userInput);
     this.userInput = userInput;
-  }
+    this.translationProvider.getTranslationResult(userInput).subscribe(
+      (response) => {
+        console.log(response);
+        this.result = response.responseData.translatedText;
+    });
+}
 }
